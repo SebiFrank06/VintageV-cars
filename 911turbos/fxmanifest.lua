fdc87e0a9e4117a86f15c29f8caaf12e5e4bc465
SebiFrank06 <79604726+SebiFrank06@users.noreply.github.com>
2021-02-24 21:53:59 +0100

    Add files via upload

---
+++ 911turbos/fxmanifest.lua
@@ -0,0 +1,21 @@
+fx_version 'adamant'
+
+game 'gta5'
+
+files {
+    'vehicles.meta',
+	'carvariations.meta',
+	'carcols.meta',
+	'vehiclelayouts.meta',
+    'handling.meta',
+	'smgcontentunlocks.meta',
+	'shop_vehicle.meta'
+}
+
+data_file 'HANDLING_FILE' 'handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'vehicles.meta'
+data_file 'CARCOLS_FILE' 'carcols.meta'
+data_file 'CAR_VARIATIONS_FILE' 'carvariations.meta'
+data_file 'SMG_CONTENT_UNLOCKS_FILE' 'smgcontentunlocks.meta'
+data_file 'SHOP_VEHICLE_FILE' 'shop_vehicle.meta'
+data_file 'VEHICLE_LAYOUTS_FILE' 'vehiclelayouts.meta'
